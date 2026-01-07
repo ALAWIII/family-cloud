1f4f0687fd3d4c008cc3bf4d5ca29b470d67fe88
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-01-07 12:17:59 +0300

    test: modifing the database connection test

--- tests/database/db.rs
+++ tests/database/db.rs
@@ -1,9 +1,18 @@
-use family_cloud::{get_db, run};
+use family_cloud::{get_db, init_db};
 
 #[tokio::test]
 async fn db_connection() {
-    let e = run().await;
+    dotenv::dotenv().ok();
+
+    let e = init_db().await;
     assert!(e.is_ok());
-    let con = get_db();
-    assert!(!con.is_closed())
+
+    let pool = get_db();
+    assert!(!pool.is_closed()); // to indicate its still open
+
+    let result: i32 = sqlx::query_scalar("SELECT 1") // issuing a query to test if the pool is correctly connected to the database
+        .fetch_one(pool)
+        .await
+        .expect("Query failed");
+    assert_eq!(result, 1);
 }
