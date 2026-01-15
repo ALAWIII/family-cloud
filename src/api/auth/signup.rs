9015ad11d90f2ff7a08faebe9974bdd0ccb6b67c
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-01-15 20:11:16 +0300

    feat: moving verify_signup_token to signup.rs file

--- src/api/auth/signup.rs
+++ src/api/auth/signup.rs
@@ -1,10 +1,21 @@
 use crate::{
-    AppState, EmailSender, PendingAccount, SignupRequest,
+    AppState, EmailSender, PendingAccount, SignupRequest, User,
     api::{encode_token, generate_token_bytes, hash_password, hash_token},
-    store_token_redis, verification_body,
+    decode_token, insert_new_account, remove_verified_account_from_redis, search_for_user_by_email,
+    search_redis_for_token, store_token_redis, verification_body,
 };
-use axum::{Json, debug_handler, extract::State, http::status::StatusCode};
+use axum::{
+    Json, debug_handler,
+    extract::{Query, State},
+    http::status::StatusCode,
+};
+use serde::Deserialize;
 use sqlx::{PgPool, types::Uuid};
+
+#[derive(Deserialize)]
+pub struct TokenQuery {
+    pub token: String,
+}
 
 /// if email_exist is true then send an email message to tell him that his email is already signup and the token must be used to reset password if he wants too
 ///
@@ -16,10 +27,9 @@ pub(super) async fn signup(
 ) -> (StatusCode, String) {
     let from_sender = std::env::var("SMTP_FROM_ADDRESS").unwrap();
 
-    let user_id = is_email_exist(&signup_info.email, &appstate.db_pool)
-        .await
-        .expect("Failed to retrieve from database"); // sqlx database error
+    let user_id = search_for_user_by_email(&appstate.db_pool, &signup_info.email).await; // sqlx database error
     if user_id.is_none() {
+        // if email is new
         let token = generate_token_bytes(32);
         let raw_token = encode_token(&token); // used to send as a url in email message
         let hashed_token = hash_token(&token); // store in redis database
@@ -33,7 +43,6 @@ pub(super) async fn signup(
         EmailSender::default()
             .from_sender(from_sender)
             .email_recipient(signup_info.email)
-            .msg_id(raw_token)
             .subject("new account email verification".to_string())
             .email_body(email_body)
             .send_email(appstate.mail_client)
@@ -71,10 +80,25 @@ fn create_account(
     Ok(pending_account)
 }
 
-/// if it returns a value means that the email is already signedup
-async fn is_email_exist(email: &str, pool: &PgPool) -> Result<Option<Uuid>, sqlx::Error> {
-    let record: Option<_> = sqlx::query!("select id from users where email=$1", email)
-        .fetch_optional(pool)
-        .await?;
-    Ok(record.map(|v| v.id))
+#[debug_handler]
+pub async fn verify_signup_token(
+    State(appstate): State<AppState>,
+    Query(token): Query<TokenQuery>,
+) {
+    let redis_con = appstate.redis_pool.get().await.unwrap();
+    let decoded = decode_token(&token.token).unwrap();
+    let hashed_token = hash_token(&decoded);
+    let account: PendingAccount = serde_json::from_str(
+        &search_redis_for_token(&hashed_token, redis_con)
+            .await
+            .expect("fail to find a pending account"),
+    )
+    .expect("failed to deserialize");
+    let user = User::new(account.username, account.email, account.password_hash);
+    insert_new_account(user, &appstate.db_pool)
+        .await
+        .expect("Failed to insert into database");
+    remove_verified_account_from_redis(appstate.redis_pool.get().await.unwrap(), &hashed_token)
+        .await
+        .unwrap()
 }
