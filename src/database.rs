2c49e9445742efd07d0e5e645f15deed9964f5f1
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-01-15 18:28:42 +0300

    feat: adding search_for_user_by_email and insert_new_account to database.rs

--- src/database.rs
+++ src/database.rs
@@ -2,6 +2,8 @@ use std::sync::OnceLock;
 
 use sqlx::postgres::{PgPool, PgPoolOptions};
 
+use crate::User;
+
 static DB_POOL: OnceLock<PgPool> = OnceLock::new();
 
 pub async fn init_db() -> Result<(), sqlx::Error> {
@@ -17,8 +19,34 @@ pub async fn init_db() -> Result<(), sqlx::Error> {
     Ok(())
 }
 
-pub fn get_db() -> &'static PgPool {
+pub fn get_db() -> PgPool {
     DB_POOL
         .get()
         .expect("the underlying database connection is not established yet")
+        .clone()
+}
+pub async fn insert_new_account(user: User, db_pool: &PgPool) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        "INSERT INTO users (id, username, email, password_hash, created_at, storage_quota_bytes, storage_used_bytes)
+         VALUES ($1, $2, $3, $4, $5, $6, $7)",
+        user.id,
+        user.username,
+        user.email,
+        user.password_hash,
+        user.created_at,
+        user.storage_quota_bytes ,
+        user.storage_used_bytes
+    )
+    .execute(db_pool)
+    .await?;
+
+    Ok(())
+}
+
+pub async fn search_for_user_by_email(con: &PgPool, email: &str) -> Option<User> {
+    sqlx::query_as!(User, "select * from users where email=$1", email)
+        .fetch_optional(con)
+        .await
+        .ok()
+        .flatten()
 }
