77a0a2926efbdfd49dac2e1b2db2d24a041237ce
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-01-15 11:34:53 +0300

    feat: improving the signup body logic , removing the possiblity of sending password-rest emails when the email account already exist
    - if the email exist delay 100 milisecond and return 200 OK
    - if the email is new , send email verification and return 200 OK
    - return a unified message telling them if the email is new they will get an email verification

--- src/api/auth/signup.rs
+++ src/api/auth/signup.rs
@@ -1,9 +1,9 @@
 use crate::{
-    AppState, EmailSender, PendingAccount, SignupPayload, SignupRequest, TokenPayload,
+    AppState, EmailSender, PendingAccount, SignupRequest,
     api::{encode_token, generate_token_bytes, hash_password, hash_token},
-    password_reset_body, store_token_redis, verification_body,
+    store_token_redis, verification_body,
 };
-use axum::{Json, debug_handler, extract::State};
+use axum::{Json, debug_handler, extract::State, http::status::StatusCode};
 use sqlx::{PgPool, types::Uuid};
 
 /// if email_exist is true then send an email message to tell him that his email is already signup and the token must be used to reset password if he wants too
@@ -13,64 +13,49 @@ use sqlx::{PgPool, types::Uuid};
 pub(super) async fn signup(
     State(appstate): State<AppState>,
     Json(signup_info): Json<SignupRequest>,
-) {
+) -> (StatusCode, String) {
     let from_sender = std::env::var("SMTP_FROM_ADDRESS").unwrap();
 
     let user_id = is_email_exist(&signup_info.email, &appstate.db_pool)
         .await
         .expect("Failed to retrieve from database"); // sqlx database error
-    let token = generate_token_bytes(32);
-    let raw_token = encode_token(&token); // used to send as a url in email message
-    let hashed_token = hash_token(&token); // store in redis database
-    let (subject, email_body, payload) =
-        signup_email_type(user_id, &signup_info.username, &raw_token, &signup_info);
-    EmailSender::default()
-        .from_sender(from_sender)
-        .email_recipient(signup_info.email)
-        .msg_id(raw_token)
-        .subject(subject)
-        .email_body(email_body)
-        .send_email(appstate.mail_client)
-        .await;
-    store_token_redis(
-        &appstate.redis_pool,
-        hashed_token,
-        payload
-            .to_json()
-            .expect("failed to convert payload to json string"),
-        5 * 60,
-    )
-    .await
-    .expect("Failed to store token in redis");
-}
-fn signup_email_type(
-    user_id: Option<Uuid>,
-    username: &str,
-    raw_token: &str,
-    signup_info: &SignupRequest,
-) -> (String, String, SignupPayload) {
-    if let Some(user_id) = user_id {
-        return (
-            "signup existing account".to_string(),
-            password_reset_body(
-                username,
-                &format!("reset-password?token={}", raw_token),
-                5,
-                "family_cloud",
-                true,
-            ),
-            SignupPayload::Existing(TokenPayload { user_id }),
-        );
-    }
-    (
-        "new account email verification".to_string(),
-        verification_body(
-            username,
+    if user_id.is_none() {
+        let token = generate_token_bytes(32);
+        let raw_token = encode_token(&token); // used to send as a url in email message
+        let hashed_token = hash_token(&token); // store in redis database
+        let pending_account = create_account(&signup_info).unwrap();
+        let email_body = verification_body(
+            &signup_info.username,
             &format!("verify?token={}", raw_token),
             5,
             "family_cloud",
-        ),
-        SignupPayload::New(create_account(signup_info).unwrap()),
+        );
+        EmailSender::default()
+            .from_sender(from_sender)
+            .email_recipient(signup_info.email)
+            .msg_id(raw_token)
+            .subject("new account email verification".to_string())
+            .email_body(email_body)
+            .send_email(appstate.mail_client)
+            .await;
+        store_token_redis(
+            appstate
+                .redis_pool
+                .get()
+                .await
+                .expect("Failed to obtain a redis connection from the pool"),
+            hashed_token,
+            &pending_account,
+            5 * 60,
+        )
+        .await
+        .expect("Failed to store token in redis");
+    } else {
+        tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+    }
+    (
+        StatusCode::OK,
+        "If this email is new, you'll receive a verification email".to_string(),
     )
 }
 
