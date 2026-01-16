93cd8bbfe534c84d39fa28829248ab1ed06d4914
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-01-16 11:53:13 +0300

    feat: adding build_router to generate the required endpoint router + improving the AppState struct

--- src/startup.rs
+++ src/startup.rs
@@ -1,22 +1,47 @@
 use axum::Router;
+use deadpool_redis::Pool as RedisPool;
+use lettre::{AsyncSmtpTransport, Tokio1Executor};
+use sqlx::PgPool;
 use tokio::net::TcpListener;
 
-use crate::{database::init_db, init_rustfs, *};
+use crate::{database::init_db, email::get_mail_client, init_rustfs, *};
+
+#[derive(Clone)]
+pub struct AppState {
+    pub db_pool: PgPool,
+    pub rustfs_con: aws_sdk_s3::Client,
+    pub redis_pool: RedisPool,
+    pub mail_client: AsyncSmtpTransport<Tokio1Executor>,
+}
 
 //---------------------------------------server---------------------------------------
-async fn server() {
-    let app = Router::new()
+pub fn build_router() -> Router {
+    let state = AppState {
+        db_pool: get_db(),
+        rustfs_con: get_rustfs(),
+        redis_pool: get_redis_pool(),
+        mail_client: get_mail_client(),
+    };
+    Router::new()
         .merge(authentication())
+        .merge(verification_router())
         .merge(user_management())
         .merge(storage_objects())
         .merge(sharing_object())
-        .merge(storage_status());
-    let listener = TcpListener::bind("127.0.0.1:5050").await.unwrap();
-    axum::serve(listener, app).await;
+        .merge(storage_status())
+        .with_state(state)
 }
 
-pub async fn run() -> Result<(), sqlx::Error> {
+async fn start_app_server() -> Result<(), std::io::Error> {
+    let router = build_router();
+    let listener = TcpListener::bind("127.0.0.1:5050").await.unwrap();
+    axum::serve(listener, router).await
+}
+
+pub async fn run() {
     dotenv::dotenv().ok();
+    init_redis_pool().await;
     init_rustfs().await;
-    init_db().await
+    init_db().await;
+    start_app_server().await;
 }
