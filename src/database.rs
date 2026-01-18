283aae7ce40ce94754b9c4ed9d1d39eab99780bd
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-01-18 17:59:37 +0300

    feat: adding database fetching methods

--- src/database.rs
+++ src/database.rs
@@ -1,8 +1,9 @@
 use std::sync::OnceLock;
 
 use sqlx::postgres::{PgPool, PgPoolOptions};
+use uuid::Uuid;
 
-use crate::User;
+use crate::{User, UserVerification};
 
 static DB_POOL: OnceLock<PgPool> = OnceLock::new();
 
@@ -43,10 +44,43 @@ pub async fn insert_new_account(user: User, db_pool: &PgPool) -> Result<(), sqlx
     Ok(())
 }
 
-pub async fn search_for_user_by_email(con: &PgPool, email: &str) -> Option<User> {
+/// used on login to retreive user information as response to user request
+pub async fn search_for_account_by_email(con: &PgPool, email: &str) -> Option<User> {
     sqlx::query_as!(User, "select * from users where email=$1", email)
         .fetch_optional(con)
         .await
         .ok()
         .flatten()
+}
+pub async fn get_account_password_reset_info_by_email(
+    con: &PgPool,
+    email: &str,
+) -> Option<UserVerification> {
+    sqlx::query_as!(
+        UserVerification,
+        "SELECT id,username,email FROM users WHERE email = $1",
+        email
+    )
+    .fetch_optional(con)
+    .await
+    .ok()
+    .flatten()
+}
+
+pub async fn update_account_password(con: &PgPool, user_id: Uuid, password_hash: &str) {
+    sqlx::query!(
+        "UPDATE  users SET password_hash=$1 where id=$2",
+        password_hash,
+        user_id
+    )
+    .execute(con)
+    .await
+    .expect("Failed to update password");
+}
+
+pub async fn is_account_exist(con: &PgPool, email: &str) -> Option<Uuid> {
+    sqlx::query_scalar!("select id from users where email=$1", email)
+        .fetch_optional(con)
+        .await
+        .expect("Failed to execute query")
 }
