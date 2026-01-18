c88e8c77a67f18112d72c96ec9ba87ddc0ead2df
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-01-18 19:21:57 +0300

    feat: implementing cancel_change_email handler

--- src/api/auth/email_change.rs
+++ src/api/auth/email_change.rs
@@ -8,7 +8,7 @@ use crate::{
     AppState, Claims, EmailInput, EmailSender, TokenQuery, UserVerification,
     create_verification_key, decode_token, delete_token_from_redis, email_cancel_body,
     email_change_body, encode_token, generate_token_bytes, get_email_by_id, get_verification_data,
-    hash_token, is_account_exist, store_token_redis, update_account_email,
+    hash_token, is_account_exist, is_token_exist, store_token_redis, update_account_email,
 };
 
 #[debug_handler]
@@ -117,4 +117,24 @@ pub async fn verify_change_email(
     Ok(StatusCode::OK)
 }
 
-pub async fn cancel_change_email() {}
+pub async fn cancel_change_email(
+    State(appstate): State<AppState>,
+    Query(raw_token): Query<TokenQuery>,
+) -> Result<StatusCode, StatusCode> {
+    let mut redis_connection = appstate
+        .redis_pool
+        .get()
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    let token_bytes = decode_token(&raw_token.token).map_err(|_| StatusCode::UNAUTHORIZED)?;
+    let hashed_token = hash_token(&token_bytes);
+    let key = create_verification_key(crate::TokenType::EmailChange, &hashed_token);
+    let t = is_token_exist(&mut redis_connection, &key).await;
+    if !t {
+        return Err(StatusCode::NOT_FOUND);
+    }
+    delete_token_from_redis(&mut redis_connection, &key)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    Ok(StatusCode::OK)
+}
