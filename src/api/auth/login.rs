b34a29af4db14b437f646caed1862e1f01c62b44
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-01-19 12:43:44 +0300

    feat: implementing login endpoint handler

--- src/api/auth/login.rs
+++ src/api/auth/login.rs
@@ -1,32 +1,49 @@
-use argon2::{Argon2, Params, PasswordHash};
-use secrecy::{ExposeSecret, SecretBox};
-use sha3::{self, Digest};
-use sqlx::PgPool;
+use axum::{Json, debug_handler, extract::State, http::StatusCode};
+use secrecy::SecretBox;
 
-pub(super) async fn login() {}
-/// used only on login !!!
-struct Credentials {
-    pub email: String,
-    pub password: SecretBox<String>,
-}
+use crate::{
+    AppState, Credentials, LoginResponse, UserProfile, create_access_token,
+    create_verification_key, encode_token, fetch_account_info, generate_token_bytes, hash_token,
+    store_token_redis, verify_password,
+};
 
-async fn validate_credentials(credentials: Credentials, pool: &PgPool) -> Result<(), &str> {
-    let row: Option<_> = sqlx::query!(
-        "select id ,password_hash from users where username=$1",
-        credentials.email
-    )
-    .fetch_optional(pool)
-    .await
-    .expect("failed to send to database");
-    let (expected_password_hash, user_id) = match row {
-        Some(row) => (row.password_hash, row.id),
-        None => {
-            return Err("failed to retrive credentials ");
-        }
-    };
-    let expected_password_hash =
-        PasswordHash::new(&expected_password_hash).expect("failed to hash password");
-    let hasher = Argon2::default();
-    let password_hash = sha3::Sha3_256::digest(credentials.password.expose_secret().as_bytes());
-    Ok(())
+#[debug_handler]
+pub(super) async fn login(
+    State(appstate): State<AppState>,
+    Json(credentials): Json<Credentials>,
+) -> Result<Json<LoginResponse>, StatusCode> {
+    let secret_key = SecretBox::new(Box::new(std::env::var("HMAC_SECRET").unwrap()));
+    let user = fetch_account_info(&appstate.db_pool, &credentials.email)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
+        .ok_or(StatusCode::UNAUTHORIZED)?;
+    if !verify_password(&credentials.password, &user.password_hash)
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
+    {
+        return Err(StatusCode::UNAUTHORIZED);
+    }
+    //--------------------------generating refresh token--------------
+    let token_bytes = generate_token_bytes(32);
+    let refresh_token = encode_token(&token_bytes);
+    let token_hash = hash_token(&token_bytes);
+    let mut redis_con = appstate
+        .redis_pool
+        .get()
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    let key = create_verification_key(crate::TokenType::Refresh, &token_hash);
+    //-------------------------- generate access token---------------
+
+    let access_token =
+        create_access_token(&user, 60 * 15, secret_key).expect("Failed to create access token");
+    store_token_redis(&mut redis_con, &key, &user.id, 30 * 24 * 60 * 60) // storing refresh token in redis to prevent a failure from not returning it to user
+        .await
+        .unwrap();
+    let user_profile = UserProfile::from(user);
+
+    Ok(Json(LoginResponse {
+        access_token,
+        refresh_token,
+        user: user_profile,
+    }))
 }
