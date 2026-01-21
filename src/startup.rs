b1dbced748b45a07027969afb96fd99aa0d06bc6
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-01-21 14:19:44 +0300

    feat: propogating the error up stack and updating start_app_server signature

--- src/startup.rs
+++ src/startup.rs
@@ -1,3 +1,4 @@
+use anyhow::anyhow;
 use axum::Router;
 use deadpool_redis::Pool as RedisPool;
 use lettre::{AsyncSmtpTransport, Tokio1Executor};
@@ -15,33 +16,36 @@ pub struct AppState {
 }
 
 //---------------------------------------server---------------------------------------
-pub fn build_router() -> Router {
+pub fn build_router() -> Result<Router, ApiError> {
     let state = AppState {
-        db_pool: get_db(),
+        db_pool: get_db()?,
         rustfs_con: get_rustfs(),
-        redis_pool: get_redis_pool(),
-        mail_client: get_mail_client(),
+        redis_pool: get_redis_pool()?,
+        mail_client: get_mail_client()?,
     };
-    Router::new()
+    let router = Router::new()
         .merge(authentication())
-        .merge(verification_router())
         .merge(user_management())
         .merge(storage_objects())
         .merge(sharing_object())
         .merge(storage_status())
-        .with_state(state)
+        .with_state(state);
+    Ok(router)
 }
 
-async fn start_app_server() -> Result<(), std::io::Error> {
-    let router = build_router();
-    let listener = TcpListener::bind("127.0.0.1:5050").await.unwrap();
-    axum::serve(listener, router).await
+async fn start_app_server(router: Router) -> anyhow::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:5050").await?;
+    axum::serve(listener, router).await?;
+    Ok(())
 }
 
-pub async fn run() {
+pub async fn run() -> anyhow::Result<()> {
     dotenv::dotenv().ok();
-    init_redis_pool().await;
+    init_mail_client()?;
+    init_redis_pool().await?;
     init_rustfs().await;
-    init_db().await;
-    start_app_server().await;
+    init_db().await?;
+    let router = build_router()?;
+    start_app_server(router).await?;
+    Ok(())
 }
