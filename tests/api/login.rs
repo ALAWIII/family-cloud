f7751d346726a55b78acaae7056b70f53339bc50
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-01-21 18:46:22 +0300

    test: adding test for the login endpoint with different valid and invalid (password,email)

---
+++ tests/api/login.rs
@@ -0,0 +1,48 @@
+use axum_test::TestResponse;
+use family_cloud::{LoginResponse, get_db};
+
+use crate::{TestAccount, create_app, create_verified_account};
+
+async fn login(
+    email: Option<&str>,
+    pswd: Option<&str>,
+) -> anyhow::Result<(TestResponse, TestAccount)> {
+    let app = create_app().await;
+    let db_pool = get_db()?;
+    let mut user = create_verified_account(&db_pool).await;
+    let _ = email.is_some_and(|e| user.email(e));
+    let _ = pswd.is_some_and(|p| user.pswd(p));
+
+    Ok((app.login_request(&user).await, user))
+}
+
+#[tokio::test]
+async fn login_valid_credits_endpoint() -> anyhow::Result<()> {
+    let (resp, user) = login(None, None).await?;
+    resp.assert_status_ok();
+    let login_resp: LoginResponse = resp.json();
+    assert!(!login_resp.access_token.is_empty());
+    assert!(!login_resp.refresh_token.is_empty());
+    assert_eq!(login_resp.user.id, user.id);
+    Ok(())
+}
+
+#[tokio::test]
+async fn login_invalid_password() -> anyhow::Result<()> {
+    login(None, Some("8d47gf8de4g8g4"))
+        .await?
+        .0 // wrong password = unauthorized
+        .assert_status_unauthorized();
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn login_invalid_email() -> anyhow::Result<()> {
+    login(Some("titanic@fucked.com"), None)
+        .await?
+        .0 // wrong email = Not Found
+        .assert_status_not_found();
+
+    Ok(())
+}
