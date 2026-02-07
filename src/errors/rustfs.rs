5756d1c9ab619ddec516635d86a53003a1c3ee7f
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-02-07 14:16:00 +0300

    feat: adding rustfs proper error types.

---
+++ src/errors/rustfs.rs
@@ -0,0 +1,17 @@
+#[derive(Debug, thiserror::Error)]
+pub enum RustFSError {
+    #[error("Rustfs Client already initalized.")]
+    AlreadyInit,
+    #[error("Failed to get the RustFS connection,maybe not initalized yet.")]
+    Connection,
+    #[error("Folder compression failed")]
+    Compress(#[from] anyhow::Error),
+    #[error("object has changed internally, no way to retreive old versions.")]
+    ETagChanged,
+    #[error("folder is empty, nothing to stream.")]
+    EmptyFolder,
+    #[error("failed to create new user bucket: {0:?}")]
+    BucketCreate(anyhow::Error),
+    #[error("S3 operation failed: {0}")]
+    S3(anyhow::Error),
+}
