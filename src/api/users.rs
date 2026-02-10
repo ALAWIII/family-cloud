a371e374bb56fed8ffcaaf0d0de919d8dd456a39
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-02-10 15:45:18 +0300

    fix: removing unneeded api resources.

--- src/api/users.rs
+++ src/api/users.rs
@@ -1,7 +1,4 @@
-use axum::{
-    Router,
-    routing::{get, put},
-};
+use axum::{Router, routing::get};
 
 use crate::AppState;
 
@@ -9,12 +6,9 @@ use crate::AppState;
 
 async fn user_profile() {}
 async fn update_user_profile_info() {}
-async fn change_password() {}
 pub fn user_management() -> Router<AppState> {
-    Router::new()
-        .route(
-            "/api/users/me",
-            get(user_profile).patch(update_user_profile_info),
-        )
-        .route("/api/users/me/password", put(change_password))
+    Router::new().route(
+        "/api/users/me",
+        get(user_profile).patch(update_user_profile_info),
+    )
 }
