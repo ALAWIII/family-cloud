35df3b635f750c51061946ae13f5ca80ab32f282
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-02-10 14:54:13 +0300

    feat: improve upload implementation.
    1. adding main upload handler :
        1. accepts new object upload request,
        2. extracts required headers like object-type ~{file,folder}.
        3. validating the object key so that no injected malformet parts.
        3. if folder -> create it directly in database.
        4. fetch user available space.
        5. if file -> spawn multiple part uploads.
        6. start stream the file objects to RustFS.
        7. error? return Response and abort.
        8. success? fetch object metadata from rustfs and insert new object in database.
    2. adding validate file path : used to ensure that the object key does not escape its domain.
    3. extract_headers: extracts required and optional headers and fixes object key slashes `/`.
    4. stream_file_rustfs_parallel: main function to start streaming the file from body of request to RustFS.
        1. spawns body streamer that consumes body and sends the bytes to an uploader.
        2. spawns 4 concurrent uploaders to start reciveing new Parts .
        3. every result of uploaded part is sent to collect_results
        4. awaits the streamers.
        5. completes the upload.
    5. create_multipart_upload: responsible for creating multipart upload session transaction , returns the upload id.
    6. spawn_uploaders: receives stream of Parts and starts to upload them individually to RustFS , the result are sent back to collect_results function.
    7. spawn_body_streamer: responsible for consuming the axum body and collects the arrived bytes into 5MB buffer and then send them to spawn_uploader function.
    8. collect_results: receives new parts and evaluate whether to abort the process because of malformed or missing parts or finish with success.
    9. complete_upload: when all parts uploaded in success, this function responsible to commit the new file into RustFS.
    10. abort_upload: on any kind of failure this fucntion responsible to clear RustFS from unfinished uploads.
    11. upload_single_part: a tiny helper function used to stream a single 5MB part to RustFS.

--- src/api/objects/upload.rs
+++ src/api/objects/upload.rs
@@ -1,7 +1,7 @@
 use std::path::Path;
 
 use crate::{
-    ApiError, AppState, Claims, ObjectKind, ObjectRecord, fetch_object_metadata,
+    ApiError, AppState, Claims, ObjectKind, ObjectRecord, RustFSError, fetch_object_metadata,
     get_user_available_storage, insert_obj,
 };
 use anyhow::anyhow;
@@ -27,7 +27,7 @@ use tokio::sync::Semaphore;
 use tokio::sync::mpsc;
 use tokio_stream::StreamExt;
 use tokio_util::bytes::BytesMut;
-use tracing::instrument;
+use tracing::{debug, info, instrument};
 
 const PART_SIZE: usize = 5 * 1024 * 1024;
 const MAX_CONCURRENT_UPLOADS: usize = 4;
@@ -51,34 +51,43 @@ struct UploadHeaders {
     pub checksum: Option<String>,
 }
 
-#[instrument(skip_all, err)]
+#[instrument(skip_all, err,fields(
+    user_id=%claims.sub,
+    username=claims.username
+))]
 pub async fn upload(
     State(appstate): State<AppState>,
     Extension(claims): Extension<Claims>,
     headers: HeaderMap,
     body: Body,
 ) -> Result<StatusCode, ApiError> {
+    info!("uploading new file.");
     let uphead = extract_headers(&headers)?;
-    let bucket_p = format!("/{}", claims.sub);
-    let f_path = format!("{}{}", bucket_p, uphead.object_key); // assuming obj_key should start with '/'
-    validate_path(Path::new(&f_path), Path::new(&bucket_p))?;
+    info!("validating object key path structure.");
+    validate_file_path(&claims.sub.to_string(), &uphead.object_key)?;
+    //-------------------------------
     let mut obj = ObjectRecord::new(claims.sub, &uphead.object_key, true);
     if uphead.object_kind.is_folder() {
+        info!("creating new folder.");
         // if obj_kind is folder -> create the folder quickly -> grab the metadata store in database -> and return success
         insert_obj(&appstate.db_pool, obj).await?;
+        info!("folder created successfully.");
         return Ok(StatusCode::CREATED);
     }
+
     //--------------- check the available space user have . space_used + content_length >= maximum_space -> reject the upload. ---------------
+    info!("fetching user available storage to store new file.");
     let s_info = get_user_available_storage(&appstate.db_pool, claims.sub).await?;
     if s_info.storage_used_bytes + uphead.content_length.unwrap_or(0) > s_info.storage_quota_bytes {
         return Err(ApiError::Unauthorized);
     }
     let bucket = obj.bucket_name();
     //-------------------------- update object metadata ----------------------
+    info!("preparing new file object instance with metadata.");
     obj.is_folder(false);
     obj.mime_type(uphead.content_type.unwrap());
     obj.checksum_sha256(uphead.checksum.unwrap());
-
+    info!("creating new multipart upload session.");
     // if obj_kind is file -> open the body and start recive the stream of bytes for that file -> pipe directly to RustFS -> return metadata->store in database -> response success.
     let upload_id = create_multipart_upload(
         &appstate.rustfs_con,
@@ -87,7 +96,9 @@ pub async fn upload(
         obj.mime_type.as_ref().unwrap(),
     )
     .await?;
+
     // ---------------------- streaming the file
+    info!("start streaming the file to RustFS object storage.");
     let rs = stream_file_rustfs_parallel(
         &appstate.rustfs_con,
         body,
@@ -98,17 +109,32 @@ pub async fn upload(
     .await;
     //-------- abort on error --------------
     if let Err(r) = rs {
+        info!("aborting the upload.");
         abort_upload(&appstate.rustfs_con, &bucket, &obj.object_key, &upload_id).await?;
         return Err(r)?;
     }
     //--------------------- ask rustfs to complement the metadata of the object------------------
+    info!("fetch object metadata from RustFS.");
     fetch_object_metadata(&appstate.rustfs_con, &mut obj).await?;
     //-------- insert the object(file) information into the database-----------------------
+    info!("inserting new object with metadata into database.");
     insert_obj(&appstate.db_pool, obj).await?;
+    info!("new file created successfully.");
     Ok(StatusCode::CREATED)
 }
 
+/// validates a path that follows a parent path and does not escape its domain.
+fn validate_file_path(bucket: &str, key: &str) -> Result<(), RustFSError> {
+    let bucket_p = format!("/{}", bucket);
+    let f_path = format!("{}{}", bucket_p, key); // assuming obj_key should start with '/'
+    validate_path(Path::new(&f_path), Path::new(&bucket_p)).map_err(|e| {
+        RustFSError::Upload(anyhow!("invalid object_key: {}, error: {}", f_path, e))
+    })?;
+    Ok(())
+}
+/// extracts required and optional headers.
 fn extract_headers(headers: &HeaderMap) -> Result<UploadHeaders, ApiError> {
+    info!("extracting headers");
     let obj_kind = headers
         .get("Object-Type")
         .and_then(|h| h.to_str().ok())
@@ -128,6 +154,7 @@ fn extract_headers(headers: &HeaderMap) -> Result<UploadHeaders, ApiError> {
         .ok_or(ApiError::BadRequest(anyhow!("Missing Object-Key header")))?;
 
     let (cont_len, cont_type, checksum) = if obj_kind.is_folder() {
+        debug!("the object is folder");
         if !obj_key.ends_with('/') {
             obj_key.push('/');
         }
@@ -136,6 +163,7 @@ fn extract_headers(headers: &HeaderMap) -> Result<UploadHeaders, ApiError> {
         }
         (None, None, None)
     } else {
+        debug!("the object is file");
         let cont_len = headers
             .get(CONTENT_LENGTH) // Use const axum::http::header::CONTENT_LENGTH
             .and_then(|h| h.to_str().ok())
@@ -173,17 +201,24 @@ fn extract_headers(headers: &HeaderMap) -> Result<UploadHeaders, ApiError> {
     })
 }
 
-// ===== Main entry point =====
+/// # Main entry point
+/// - the function orchestrator for the whole streaming process.
+/// 1. spawn_body_streamer: collects, chunks the arrived bytes into buffer and sends them into the channel (part_tx).
+/// 2. spawn_uploaders: spawns multiple parllel uploaders, recives arrived Part's (part_rx) stream them to RustFS, the results returned are sent into the channel (result_tx).
+/// 3. collect_results: recives a stream of completed parts using (result_rx) and then collects and return back.
+/// 4.
 pub async fn stream_file_rustfs_parallel(
     client: &Client,
     body: Body,
     bucket: &str,
     key: &str,
     upload_id: &str,
-) -> anyhow::Result<()> {
+) -> Result<(), RustFSError> {
+    debug!("initalizing Parts communicating channels.");
     let (part_tx, part_rx) = mpsc::channel::<PartToUpload>(10);
-    let (result_tx, result_rx) = mpsc::channel::<Result<CompletedPart, anyhow::Error>>(10);
+    let (result_tx, result_rx) = mpsc::channel::<Result<CompletedPart, RustFSError>>(10);
 
+    debug!("spawn parallel uploaders");
     // Spawn parallel uploaders
     let uploader_handle = spawn_uploaders(
         client.clone(),
@@ -193,18 +228,20 @@ pub async fn stream_file_rustfs_parallel(
         part_rx,
         result_tx.clone(),
     );
-
+    debug!("start consuming the body and stream the arrived bytes to spawn_uploaders.");
     // Stream and buffer body into parts
     let streamer_handle = spawn_body_streamer(body, part_tx);
 
     // Collect upload results
+    debug!("droppint the result_tx<CompletePart> sender.");
     drop(result_tx); // drop the channel send None to the reciever
     let completed_parts = collect_results(result_rx).await?; // if it receieves an error will hang everything
 
     // Wait for tasks
+    debug!("waiting streamer and uploader handles to finish.");
     let _ = streamer_handle.await;
     let _ = uploader_handle.await;
-
+    debug!("commiting and validating the uploaded file.");
     // Complete upload
     complete_upload(client, bucket, key, upload_id, completed_parts).await
 }
@@ -215,42 +252,56 @@ async fn create_multipart_upload(
     bucket: &str,
     key: &str,
     obj_type: &str,
-) -> anyhow::Result<String> {
+) -> Result<String, RustFSError> {
     let response = client
         .create_multipart_upload()
         .bucket(bucket)
         .key(key)
         .content_type(obj_type)
         .send()
-        .await?;
+        .await
+        .map_err(|e| {
+            RustFSError::Upload(anyhow!(
+                "failed to create multipart upload transaction: {}",
+                e
+            ))
+        })?;
 
     response
         .upload_id()
         .map(|id| id.to_string())
-        .ok_or_else(|| anyhow!("No upload ID returned"))
+        .ok_or_else(|| RustFSError::Upload(anyhow!("No upload ID returned")))
 }
 
-// ===== Spawn parallel uploader tasks =====
+/// ===== Spawn parallel uploader tasks =====
+/// # Concurrent Uploading
+/// - consumes the part_rx receive channel endpoint
+/// - new part arraived 5MB -> acquire a permit -> spawn new upload thread -> fead to upload_single_part -> start stream the part to RustFS.
+/// - returns a result -> stream it back using result_tx send endpoint channel.
+/// - drop the permit to free a slot.
 fn spawn_uploaders(
     client: Client,
     bucket: String,
     key: String,
     upload_id: String,
     mut part_rx: mpsc::Receiver<PartToUpload>,
-    result_tx: mpsc::Sender<Result<CompletedPart, anyhow::Error>>,
+    result_tx: mpsc::Sender<Result<CompletedPart, RustFSError>>,
 ) -> tokio::task::JoinHandle<()> {
     tokio::spawn(async move {
         let semaphore = Arc::new(Semaphore::new(MAX_CONCURRENT_UPLOADS));
         let mut handles = vec![];
         // if None means the sender is dropped and streaming has finished.
         while let Some(part) = part_rx.recv().await {
+            debug!(
+                "receiving a new Part to upload. part no: {}",
+                part.part_number
+            );
             let permit = semaphore.clone().acquire_owned().await.unwrap();
             let client = client.clone();
             let bucket = bucket.clone();
             let key = key.clone();
-            let upload_id = upload_id.clone();
             let result_tx = result_tx.clone();
-
+            let upload_id = upload_id.clone();
             let handle = tokio::spawn(async move {
                 // sending the individual byte of data
                 let result = upload_single_part(
@@ -262,23 +313,30 @@ fn spawn_uploaders(
                     part.data,
                 )
                 .await;
-
+                debug!(
+                    "redirecting the result of a part to result channel. part no: {}",
+                    part.part_number
+                );
                 drop(permit);
                 let _ = result_tx.send(result).await;
             });
-
+            debug!("storing new upload part handle.");
             handles.push(handle); // storing for later awaiting all payloads to be sent
         }
+        debug!("awaiting all parts to finish uploading.");
         join_all(handles).await;
     })
 }
 
-// ===== Stream body and buffer into parts =====
+/// # ===== Stream body and buffer into parts =====
+/// 1. collects bytes from  body into buffer.
+/// 2. len(buffer) = 5MB -> wrap the 5MB bytes into Part type and send it using part_tx
 fn spawn_body_streamer(
     body: Body,
     part_tx: mpsc::Sender<PartToUpload>,
-) -> tokio::task::JoinHandle<Result<(), anyhow::Error>> {
+) -> tokio::task::JoinHandle<Result<(), RustFSError>> {
     tokio::spawn(async move {
+        debug!("consuming body.");
         let mut stream = body.into_data_stream();
         let mut buffer = BytesMut::new();
         let mut part_number = 1;
@@ -288,12 +346,15 @@ fn spawn_body_streamer(
             while buffer.len() < PART_SIZE {
                 match stream.next().await {
                     Some(Ok(chunk)) => buffer.extend_from_slice(&chunk),
-                    Some(Err(e)) => return Err(anyhow!("Stream error: {}", e)),
+                    Some(Err(e)) => {
+                        return Err(RustFSError::Upload(anyhow!("Stream error: {}", e)));
+                    }
                     None => break,
                 }
             }
 
             if buffer.is_empty() {
+                debug!("buffer is empty.");
                 break;
             }
 
@@ -305,13 +366,10 @@ fn spawn_body_streamer(
             };
 
             // Send to uploaders
-            if part_tx
+            part_tx
                 .send(PartToUpload { part_number, data })
                 .await
-                .is_err()
-            {
-                return Err(anyhow!("Upload channel closed"));
-            }
+                .map_err(|_| RustFSError::Upload(anyhow!("Upload channel closed")))?;
 
             part_number += 1;
         }
@@ -322,14 +380,17 @@ fn spawn_body_streamer(
 
 // ===== Collect upload results =====
 async fn collect_results(
-    mut result_rx: mpsc::Receiver<Result<CompletedPart, anyhow::Error>>,
-) -> anyhow::Result<Vec<CompletedPart>> {
+    mut result_rx: mpsc::Receiver<Result<CompletedPart, RustFSError>>,
+) -> Result<Vec<CompletedPart>, RustFSError> {
+    debug!("collecting parts.");
     let mut completed_parts = Vec::new();
 
     while let Some(part) = result_rx.recv().await {
-        completed_parts.push(part?);
+        completed_parts
+            .push(part.map_err(|e| RustFSError::Upload(anyhow!("corrupted part: {}", e)))?);
     }
     completed_parts.sort_by_key(|p| p.part_number);
+    debug!("collecting parts and sorting them success.");
     Ok(completed_parts)
 }
 
@@ -340,7 +401,7 @@ async fn complete_upload(
     key: &str,
     upload_id: &str,
     completed_parts: Vec<CompletedPart>,
-) -> anyhow::Result<()> {
+) -> Result<(), RustFSError> {
     let completed = CompletedMultipartUpload::builder()
         .set_parts(Some(completed_parts))
         .build();
@@ -353,7 +414,7 @@ async fn complete_upload(
         .multipart_upload(completed)
         .send()
         .await
-        .map_err(|e| anyhow!("Complete upload failed: {}", e))?;
+        .map_err(|e| RustFSError::Upload(anyhow!("Complete upload failed: {}", e)))?;
 
     Ok(())
 }
@@ -364,7 +425,7 @@ async fn abort_upload(
     bucket: &str,
     key: &str,
     upload_id: &str,
-) -> anyhow::Result<()> {
+) -> Result<(), RustFSError> {
     client
         .abort_multipart_upload()
         .bucket(bucket)
@@ -372,7 +433,7 @@ async fn abort_upload(
         .upload_id(upload_id)
         .send()
         .await
-        .map_err(|e| anyhow!("Abort upload failed: {}", e))?;
+        .map_err(|e| RustFSError::Upload(anyhow!("Abort upload failed: {}", e)))?;
 
     Ok(())
 }
@@ -385,7 +446,7 @@ async fn upload_single_part(
     upload_id: &str,
     part_number: i32,
     data: Bytes,
-) -> anyhow::Result<CompletedPart> {
+) -> Result<CompletedPart, RustFSError> {
     for attempt in 1..=3 {
         match client
             .upload_part()
@@ -398,21 +459,22 @@ async fn upload_single_part(
             .await
         {
             Ok(output) => {
+                debug!("uploading success {}", part_number);
                 return Ok(CompletedPart::builder()
                     .part_number(part_number)
                     .e_tag(output.e_tag.unwrap_or_default())
                     .build());
             }
-            Err(e) if attempt < 3 => {
+            Err(_) if attempt < 3 => {
                 tokio::time::sleep(tokio::time::Duration::from_secs(attempt * 2)).await;
             }
             Err(e) => {
-                return Err(anyhow!(
+                return Err(RustFSError::Upload(anyhow!(
                     "Part {} failed after {} retries: {}",
                     part_number,
                     attempt,
                     e
-                ));
+                )));
             }
         }
     }
