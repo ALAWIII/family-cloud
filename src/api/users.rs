571ebacfb3984605f77d0f403831e52f23a64b8d
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-03-13 16:49:55 +0300

    refactor: move fetch_storage_info from storage.rs to users.rs

--- src/api/users.rs
+++ src/api/users.rs
@@ -1,32 +1,22 @@
 use aws_sdk_s3::error::ProvideErrorMetadata;
-use axum::{Extension, Json, Router, extract::State, middleware::from_fn_with_state, routing::get};
+use axum::{Extension, Json, extract::State};
 use axum_extra::extract::CookieJar;
-use secrecy::{ExposeSecret, SecretString};
+use secrecy::ExposeSecret;
 use serde::{Deserialize, Serialize};
 
 use crate::{
     ApiError, AppState, Claims, DeleteJob, RustFSError, TokenPayload, UserProfile,
-    delete_account_db, delete_user_bucket, extract_refresh_token, fetch_profile_info,
-    get_redis_con, revoke_refresh_token, send_delete_jobs_to_worker, update_account_username,
-    validate_jwt_access_token,
+    api::objects::UserStorageInfo, delete_account_db, delete_user_bucket, extract_refresh_token,
+    fetch_profile_info, get_redis_con, get_user_available_storage, revoke_refresh_token,
+    send_delete_jobs_to_worker, update_account_username,
 };
 use tracing::{info, instrument};
 //------------------------------------user management-------
 
-pub fn user_management(hmac: SecretString) -> Router<AppState> {
-    Router::new()
-        .route(
-            "/api/users/me",
-            get(user_profile)
-                .patch(update_user_username)
-                .delete(delete_account),
-        )
-        .layer(from_fn_with_state(hmac, validate_jwt_access_token))
-}
 #[instrument(skip_all,fields(
     user_id=%claims.sub
 ))]
-async fn user_profile(
+pub(super) async fn user_profile(
     Extension(claims): Extension<Claims>,
     State(appstate): State<AppState>,
 ) -> Result<Json<UserProfile>, ApiError> {
@@ -40,7 +30,7 @@ async fn user_profile(
     new_username=username.user_name,
 
 ))]
-async fn update_user_username(
+pub(super) async fn update_user_username(
     Extension(claims): Extension<Claims>,
     State(appstate): State<AppState>,
     Json(mut username): Json<UpdateUserNameOps>,
@@ -65,7 +55,7 @@ pub struct UpdateUserNameOps {
 #[instrument(skip_all,fields(
     user_id=%claims.sub,
 ))]
-async fn delete_account(
+pub(super) async fn delete_account(
     Extension(claims): Extension<Claims>,
     State(appstate): State<AppState>,
     cookie_jar: CookieJar,
@@ -102,4 +92,15 @@ async fn delete_account(
     revoke_refresh_token(hmac_sec, refresh_token.expose_secret(), &mut redis_con).await?;
     info!("delete account success.");
     Ok(())
+}
+
+#[instrument(skip_all, fields(
+    user_id=%claims.sub
+))]
+pub(super) async fn fetch_storage_info(
+    Extension(claims): Extension<Claims>,
+    State(appstate): State<AppState>,
+) -> Result<Json<UserStorageInfo>, ApiError> {
+    let s = get_user_available_storage(&appstate.db_pool, claims.sub).await?;
+    Ok(Json(s))
 }
